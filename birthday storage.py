74922181bdbab9d50ffd83f050cf0c6db869707e
daniel2009cod <daniel.shlomov.2009@gmail.com>
2025-03-17 18:06:10 +0200

    birthday storage

---
+++ birthday storage.py
@@ -0,0 +1,16 @@
+birthday1 = int(input("enter your birthday"))
+birthday2 = int(input("enter your birthday"))
+birthday3 = int(input("enter your birthday"))
+birthday4 = int(input("enter your birthday"))
+birthday5 = int(input("enter your birthday"))
+
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
+
+
+
+
+
